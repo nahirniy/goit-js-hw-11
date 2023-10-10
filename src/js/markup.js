3b9d9f19aa129c5ec08185eea286dfe36192a99f
nahirniy <nahirniyy@gmail.com>
2023-10-10 23:16:21 +0300

    update mistakes

---
+++ src/js/markup.js
@@ -0,0 +1,30 @@
+export function createMarkup(arrayItem) {
+  return (markup = arrayItem
+    .map(
+      ({ tags, webformatURL, largeImageURL, views, downloads, likes, comments }) =>
+        `<div class="photo-card">
+            <a class="gallery-link" href="${largeImageURL}">
+                <img src="${webformatURL}" alt="${tags}" loading="lazy"/>
+                <div class="info">
+                    <p class="info-item">
+                        <b>Likes</b>
+                        ${likes}
+                    </p>
+                    <p class="info-item">
+                        <b>Views</b>
+                        ${views}
+                    </p>
+                    <p class="info-item">
+                        <b>Comments</b>
+                        ${comments}
+                    </p>
+                    <p class="info-item">
+                        <b>Downloads</b>
+                        ${downloads}
+                    </p>
+                </div>
+            </a>
+        </div>`
+    )
+    .join(''));
+}
--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,16 @@
-import axios from 'axios';
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
+import { createMarkup } from './markup.js';
+import { getRequest } from './pixabay-api.js';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const searchInput = document.querySelector('.search-input');
 const searchForm = document.querySelector('.search-form');
 const galleryBox = document.querySelector('.gallery');
 const targetBox = document.querySelector('.observe-box');
-const BASE_URL = 'https://pixabay.com/api/';
-let counterOfPage = 0;
+
+let searchItem = '';
+let page = 1;
 
 let lightbox = new SimpleLightbox('.gallery-link', {
   captionDelay: 250,
@@ -21,98 +23,72 @@ let options = {
   threshold: 1.0,
 };
 
+let observer = new IntersectionObserver(onLoad, options);
+
+function onLoad(entries, observer) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      getRequest(searchItem, (page += 1)).then(
+        ({ data: { hits: arrayCards, totalHits: totalCards } }) => {
+          if (page > Math.ceil(totalCards / 40)) {
+            observer.unobserve(targetBox);
+            Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
+          }
+
+          const markup = createMarkup(arrayCards);
+          galleryBox.insertAdjacentHTML('beforeend', markup);
+
+          lightbox.refresh();
+        }
+      );
+    }
+  });
+}
+
 function onSubmit(e) {
   e.preventDefault();
+  page = 1;
   galleryBox.innerHTML = '';
-  const searchItem = searchInput.value;
-  let observer = new IntersectionObserver(onLoad, options);
+
+  searchItem = searchInput.value;
 
   if (!searchItem) {
     Notiflix.Notify.failure(`You have to write something in the search`);
     return;
   }
 
-  async function getRequest() {
-    const response = await axios.get(`${BASE_URL}`, {
-      params: {
-        key: '39900085-18543262e84803b81288c3331',
-        q: searchItem,
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: true,
-        page: (counterOfPage += 1),
-        per_page: 40,
-      },
-    });
-
-    return response;
-  }
-
-  getRequest()
-    .then(({ data: { hits, totalHits } }) => {
-      if (hits.length === 0) {
+  getRequest(searchItem, page)
+    .then(({ data: { hits: arrayCards, totalHits: totalCards } }) => {
+      if (arrayCards.length === 0) {
         throw new Error();
       }
+
+      const markup = createMarkup(arrayCards);
+      galleryBox.insertAdjacentHTML('beforeend', markup);
+
+      Notiflix.Notify.success(`Hooray! We found ${totalCards} images.`);
+
+      lightbox.refresh();
+
       observer.observe(targetBox);
-      markup(hits);
-      Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
+
+      const { height: cardHeight } = document
+        .querySelector('.gallery')
+        .firstElementChild.getBoundingClientRect();
+
+      window.scrollBy({
+        top: cardHeight * 2,
+        behavior: 'smooth',
+      });
     })
+
     .catch(() =>
-      Notiflix.Notify.failure(
-        'Sorry, there are no images matching your search query. Please try again.'
-      )
+      Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.')
     );
-
-  function onLoad(entries, observer) {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        getRequest().then(({ data: { hits, totalHits } }) => {
-          if (counterOfPage > totalHits / 40) {
-            observer.unobserve(targetBox);
-          }
-          markup(hits);
-          lightbox.refresh();
-        });
-      }
-    });
-  }
 }
-
 searchForm.addEventListener('submit', onSubmit);
 
-function markup(arrayItem) {
-  const markup = arrayItem
-    .map(
-      ({ tags, webformatURL, largeImageURL, views, downloads, likes, comments }) =>
-        `<div class="photo-card">
-            <a class="gallery-link" href="${largeImageURL}">
-                <img src="${webformatURL}" alt="${tags}" loading="lazy"/>
-                <div class="info">
-                    <p class="info-item">
-                        <b>Likes</b>
-                        ${likes}
-                    </p>
-                    <p class="info-item">
-                        <b>Views</b>
-                        ${views}
-                    </p>
-                    <p class="info-item">
-                        <b>Comments</b>
-                        ${comments}
-                    </p>
-                    <p class="info-item">
-                        <b>Downloads</b>
-                        ${downloads}
-                    </p>
-                </div>
-            </a>
-        </div>`
-    )
-    .join('');
-  galleryBox.insertAdjacentHTML('beforeend', markup);
-
-  lightbox.refresh();
-}
+// mini-style for form
 
 window.addEventListener('scroll', () => {
   if (window.scrollY > 100 && !(document.activeElement === searchInput)) {
